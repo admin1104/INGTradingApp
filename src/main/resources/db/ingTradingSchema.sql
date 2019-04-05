b05731afd1d32a1f6b3b4e7090056f2c5873cee4
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-04-05 19:16:14 +0530

    Stock DB scripts

--- src/main/resources/db/ingTradingSchema.sql
+++ src/main/resources/db/ingTradingSchema.sql
@@ -1,13 +1,14 @@
 create database ingtradedb;
 
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(1,"Stock1");
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(2,"Stock2");
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(3,"Stock3");
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(4,"Stock4");
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(5,"Stock5");
+INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(1,"HCL");
+INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(2,"INF");
+INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(3,"TCS");
+INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(4,"CTS");
+INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(5,"WIP");
 
 
 INSERT INTO `ingtradedb`.`user`(`user_id`,`user_name`) VALUES (1,'Advitha');
 INSERT INTO `ingtradedb`.`user`(`user_id`,`user_name`) VALUES (2,'Mohan');
 INSERT INTO `ingtradedb`.`user`(`user_id`,`user_name`) VALUES (3,'Chandan');
 INSERT INTO `ingtradedb`.`user`(`user_id`,`user_name`) VALUES (4,'Teju');
+
