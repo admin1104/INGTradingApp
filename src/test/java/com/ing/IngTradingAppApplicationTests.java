1f107952b21ead7e6723798f32da642640cff445
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-04-07 16:03:03 +0530

    property file remote DB changes

--- src/test/java/com/ing/IngTradingAppApplicationTests.java
+++ src/test/java/com/ing/IngTradingAppApplicationTests.java
@@ -1,6 +1,5 @@
 package com.ing;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,8 +8,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class IngTradingAppApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	/*
+	 * @Test public void contextLoads() { }
+	 */
 
 }
