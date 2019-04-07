0a11e08e101c287646de8956b838f504ca6106ff
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-04-07 17:05:55 +0530

    DB scripts

--- src/main/resources/db/ingTradingSchema.sql
+++ src/main/resources/db/ingTradingSchema.sql
@@ -1,14 +1,14 @@
-create database ingtradedb;
+create database ingdb;
 
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`,'description') VALUES(1,"HCL","HCL Technologies Limited is engaged in providing a range of software development services, business process outsourcing services and information technology infrastructure services.");
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`,'description') VALUES(2,"INF","Infosys Limited is an Indian multinational corporation that provides business consulting, information technology and outsourcing services.");
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(3,"TCS");
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(4,"CTS");
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`,'description') VALUES(5,"WIP","Wipro Limited is an Indian multinational corporation that provides information technology, consulting and business process services.");
+INSERT INTO `ingdb`.`stock`(`stock_id`,`description`,`stock_name`) VALUES(1,'HCL Technologies Limited is engaged in providing a range of software development services, business process out sourcing services and information technology infrastructure services.','HCL');
+INSERT INTO `ingdb`.`stock`(`stock_id`,`description`,`stock_name`) VALUES(2,'Infosys Limited is an Indian multinational corporation that provides business consulting, information technology and out sourcing services.','INF');
+INSERT INTO `ingdb`.`stock`(`stock_id`,`description`,`stock_name`) VALUES(3,'Tata Consultancy Services is a global leader in IT services, consulting & business solutions with a large network of innovation and delivery centers','TCS');
+INSERT INTO `ingdb`.`stock`(`stock_id`,`description`,`stock_name`) VALUES(4,'A Certified Technology Specialist (CTS) performs general technology solution tasks by: Creating, operating, and servicing AV solutions.','CTS');
+INSERT INTO `ingdb`.`stock`(`stock_id`,`description`,`stock_name`) VALUES(5,'Wipro Limited is an Indian multinational corporation that provides information technology, consulting and business process services.','WIP');
 
 
-INSERT INTO `ingtradedb`.`user`(`user_id`,`user_name`) VALUES (1,'Advitha');
-INSERT INTO `ingtradedb`.`user`(`user_id`,`user_name`) VALUES (2,'Mohan');
-INSERT INTO `ingtradedb`.`user`(`user_id`,`user_name`) VALUES (3,'Chandan');
-INSERT INTO `ingtradedb`.`user`(`user_id`,`user_name`) VALUES (4,'Teju');
+INSERT INTO `ingdb`.`user`(`user_id`,`user_name`) VALUES (1,'Advitha');
+INSERT INTO `ingdb`.`user`(`user_id`,`user_name`) VALUES (2,'Mohan');
+INSERT INTO `ingdb`.`user`(`user_id`,`user_name`) VALUES (3,'Chandan');
+INSERT INTO `ingdb`.`user`(`user_id`,`user_name`) VALUES (4,'Teju');
 
