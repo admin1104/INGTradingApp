6598fa73e329d3f03c16e8d0f36a2a95b4c87860
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-04-07 15:18:34 +0530

    stock analytics

--- src/main/resources/db/ingTradingSchema.sql
+++ src/main/resources/db/ingTradingSchema.sql
@@ -1,10 +1,10 @@
 create database ingtradedb;
 
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(1,"HCL");
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(2,"INF");
+INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`,'description') VALUES(1,"HCL","HCL Technologies Limited is engaged in providing a range of software development services, business process outsourcing services and information technology infrastructure services.");
+INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`,'description') VALUES(2,"INF","Infosys Limited is an Indian multinational corporation that provides business consulting, information technology and outsourcing services.");
 INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(3,"TCS");
 INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(4,"CTS");
-INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`) VALUES(5,"WIP");
+INSERT INTO `ingtradedb`.`stock`(`stock_id`,`stock_name`,'description') VALUES(5,"WIP","Wipro Limited is an Indian multinational corporation that provides information technology, consulting and business process services.");
 
 
 INSERT INTO `ingtradedb`.`user`(`user_id`,`user_name`) VALUES (1,'Advitha');
