6b53f55562a960d7e2dda5551e388ad446eaf771
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-04-07 05:36:25 -0700

    query change

--- src/main/java/com/ing/repository/StockDetailsRepository.java
+++ src/main/java/com/ing/repository/StockDetailsRepository.java
@@ -13,7 +13,7 @@ public interface StockDetailsRepository extends JpaRepository<StockDetails, Long
 	
 	public List<StockDetails> findByUserId(long userId);
 	
-	@Query(value="select sd.stock_name, SUM(sd.units) as units from stock_details sd where DATE(sd.purchase_date)=DATE(NOW()) group by sd.stock_name where purchage_date", nativeQuery = true)
+	@Query(value="select sd.stock_name, SUM(sd.units) as units from stock_details sd where DATE(sd.purchase_date)=DATE(NOW()) group by sd.stock_name where purchase_date", nativeQuery = true)
 	public List<Object[]> getDailyStockDetails();
 	
 
